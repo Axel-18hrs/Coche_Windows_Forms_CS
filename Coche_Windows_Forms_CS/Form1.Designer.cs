2ce69614b5f17042f6645594f9d72030b94e7a05
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-03-13 23:49:48 -0600

    actualizando

--- Coche_Windows_Forms_CS/Form1.Designer.cs
+++ Coche_Windows_Forms_CS/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dgvListaAutos = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgvListaAutos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvListaAutos
+            // 
+            dgvListaAutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvListaAutos.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4 });
+            dgvListaAutos.Location = new Point(545, 88);
+            dgvListaAutos.Name = "dgvListaAutos";
+            dgvListaAutos.RowHeadersVisible = false;
+            dgvListaAutos.RowHeadersWidth = 51;
+            dgvListaAutos.RowTemplate.Height = 29;
+            dgvListaAutos.Size = new Size(653, 361);
+            dgvListaAutos.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Marca";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Modelo";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Motor";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Color";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            Column4.Width = 125;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(84, 213);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(240, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1230, 509);
+            Controls.Add(comboBox1);
+            Controls.Add(dgvListaAutos);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvListaAutos).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dgvListaAutos;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private ComboBox comboBox1;
     }
 }
